00c3106333300eaf470c9dd8b1319f216cc4e964
hossein hosseini <hosseinh1075@gmail.com>
2025-11-25 10:13:11 +0330

    add css style nav Header desktop

--- src/Components/Header/NavDesktop/NavDesktop.jsx
+++ src/Components/Header/NavDesktop/NavDesktop.jsx
@@ -7,30 +7,30 @@ import { BiCommentDetail } from "react-icons/bi";
 function NavDesktop() {
     return (
         <>
-            <nav>
-                <div>
-                    <button>
+            <nav className='flex items-center justify-between py-2 px-10 bg-white'>
+                <div className='flex gap-4'>
+                    <button className='text-white bg-emerald-500 w-10 h-9 flexCenter rounded-md cursor-pointer'>
                         <FaBars />
                     </button>
-                    <div>
-                        <input type="text" placeholder='Search' />
-                        <span>
-                            <IoSearchOutline />
+                    <div className='relative bg-gray-100 h-9 flexCenter border-2 border-solid border-violet-200 rounded-md'>
+                        <input type="text" className='w-full outline-none pl-7' placeholder='Search' />
+                        <span className='absolute left-1'>
+                            <IoSearchOutline size={20}/>
                         </span>
                     </div>
                 </div>
-                <div>
-                    <button>
-                        <MdOutlineLightMode />
+                <div className='flex items-center'>
+                    <button className='size-10 rounded-full flexCenter border-2 border-solid border-zinc-200 cursor-pointer'>
+                        <MdOutlineLightMode size={20}/>
                     </button>
-                    <button>
-                        <BiCommentDetail />
+                    <button className='size-10 rounded-full flexCenter border-2 border-solid border-zinc-200 cursor-pointer ml-2'>
+                        <BiCommentDetail size={20}/>
                     </button>
-                    <div>
-                        <img src="/images/header/user2.png" alt="profile" />
-                        <div>
+                    <div className='flex ml-5'>
+                        <img src="/images/header/user2.png"  className='size-12 rounded-full' alt="profile" />
+                        <div className='flex flex-col ml-2 cursor-pointer text-zinc-800'>
                             <span>Zoya Khan</span>
-                            <span>Admin</span>
+                            <span className='text-xs'>Admin</span>
                         </div>
                     </div>
                 </div>
