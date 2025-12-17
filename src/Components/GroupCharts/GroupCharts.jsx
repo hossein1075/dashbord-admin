d7d276ec68c076446a8cfa78e78930a34a18daeb
hossein hosseini <hosseinh1075@gmail.com>
2025-12-17 11:45:58 +0330

    add css part1 components group Chart in home pages

--- src/Components/GroupCharts/GroupCharts.jsx
+++ src/Components/GroupCharts/GroupCharts.jsx
@@ -4,11 +4,19 @@ import TitlePage from '../TitlePage/TitlePage'
 import { FiBarChart } from "react-icons/fi";
 import { FaArrowTrendUp } from "react-icons/fa6";
 import Earning from './Earning';
+
+const incomeInformation = [
+    {name:'Total Sales', rate: '800', price: '-$209', color: 'text-red-500', rateIcon: '30.9'},
+    {name:'Revenue', rate: '6200', price: '+$980', color: 'text-emerald-500', rateIcon: '56.2'},
+    {name:'Products', rate: '630', price: '+$46', color: 'text-emerald-500', rateIcon: '28.8'},
+    {name:'Ads Spent', rate: '$380', price: '-$60', color: 'text-red-500', rateIcon: '49.3'},
+    {name:'Expenses', rate: '890', price: '+$498', color: 'text-emerald-500', rateIcon: '3.9'},
+]
 function GroupCharts() {
     return (
         <>
-            <div className='grid grid-cols-2 my-7 gap-6'>
-                <div className='style-div'>
+            <div className='flex w-full my-7 gap-6'>
+                <div className='style-div basis-[40%]'>
                     <h2 className='mb-2 text-lg font-Noto-SemiBold font-semibold'>Earnings</h2>
                     <div>
                         <div>
@@ -17,12 +25,14 @@ function GroupCharts() {
                         <div></div>
                     </div>
                 </div>
-                <div className='style-div'>
+                <div className='style-div basis-[60%]'>
                     <h2 className='mb-2 text-lg font-Noto-SemiBold font-semibold'>Weekly Income</h2>
                     <div className='grid grid-cols-2'>
                         <ChartsBar />
                         <div className='border-2 border-solid border-zinc-200 rounded-md py-4 px-1'>
-                            <div className='flex justify-between items-center'>
+                           {incomeInformation.map((item,index) => {
+                            return (
+                                 <div className='flex justify-between items-center mb-6'>
                                 <div className='flex items-center gap-1'>
                                     <span className='bg-cyan-100 flexCenter size-12 rounded-full'>
                                         <FiBarChart size={20} color='#04aa6d' />
@@ -40,6 +50,8 @@ function GroupCharts() {
                                     </p>
                                 </div>
                             </div>
+                            )
+                           })}
                         </div>
                     </div>
                 </div>
