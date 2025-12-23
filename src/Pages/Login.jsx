fc52c850e3e28d59e44dca8a858cc7204d3b3c66
hossein hosseini <hosseinh1075@gmail.com>
2025-12-23 10:52:34 +0330

    add basic css style form login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,27 +6,29 @@ import { Link } from 'react-router-dom';
 function Login() {
   return (
     <>
-   <div>
-    <h1>Login</h1>
-    <h3>Please enter your Login and your Password</h3>
+   <div className='text-center text-white'>
+    <h1 className='font-Noto-Bold text-40px mb-0.5'>Login</h1>
+    <h3 className=''>Please enter your Login and your Password</h3>
      <form action="#">
-      <div>
-        <FaRegUser />
-        <input type="text" placeholder='UserName'/>
+      <div className='text-left h-9 flexCenter gap-2 rounded-xl border-2 border-solid border-white'>
+        <span className='ml-2'>
+          <FaRegUser size={25}/>
+        </span>
+        <input type="text" className='w-full outline-none placeholder:text-sm' placeholder='UserName or Email'/>
       </div>
-      <div>
-        <FiLock />
-        <input type="password" placeholder='Password'/>
+      <div className='text-left  h-9 flexCenter gap-2 rounded-xl border-2 border-solid border-white'>
+        <span className='ml-2'>
+          <FiLock size={25}/>
+        </span>
+        <input type="password" className='w-full outline-none placeholder:text-sm' placeholder='Password'/>
       </div>
-      <div>
-        <button>Login</button>
-      </div>
+        <button className='w-full h-9 flexCenter rounded-xl border-2 border-solid border-white text-xl'>Login</button>
     </form>
-    <button>
-      <LoginSVG/>
-      Or, sign-in with Google
+    <button className='w-full h-9 flexCenter gap-2.5 rounded-xl bg-zinc-900'>
+      <LoginSVG size={18}/>
+      <span className='text-sm'>Or, sign-in with Google</span>
     </button>
-    <Link>Forgot password?</Link>
+    <Link to='#' className='block text-xs text-indigo-500 font-Noto-SemiBold text-left'>Forgot password?</Link>
    </div>
     </>
   )
